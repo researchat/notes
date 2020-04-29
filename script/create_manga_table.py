e8681a49401dcce3102e90307c0f39bfaf45adb3
Satoshi Tamaki <coela.st@gmail.com>
2020-04-29 17:58:50 +0900

    minoro update

--- script/create_manga_table.py
+++ script/create_manga_table.py
@@ -7,8 +7,8 @@ print ("| ------ | ----- | ---- | -------- | ------------- |")
  
 for i, s in enumerate(tsv):
     eplink = []
-    for t in s[-1].split(','):
-        eplink.append("[{}](https://researchat.fm/episode/{})".format(s[1],s[1]))
-    print ("|" + '[{}]({})'.format(s[0],s[2]) + '|' + '<a href="{}" > <img src="{}" height=120 alt="{}"></a>'.format(s[2],s[1],s[0]) + "|" + s[3] + "|" + s[3] + "|" + " ".join(eplink) + "|")
+    for t in s[5].split(','):
+        eplink.append("[{}](https://researchat.fm/episode/{})".format(s[5],s[5]))
+    print ("|" + '[{}]({})'.format(s[0],s[2]) + '|' + '<a href="{}" > <img src="{}" height=120 alt="{}"></a>'.format(s[2],s[1],s[0]) + "|" + s[3] + "|" + s[4] + "|" + " ".join(eplink) + "|")
         
 
